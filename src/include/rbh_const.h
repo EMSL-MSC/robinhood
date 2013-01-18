2a065d7d77013efd9b30aac809c2914162119699
Evan J. Felix <karcaw@gmail.com>
2013-01-18 10:57:49 -0800

    Revert "Set RBH_PATH_MAX to system PATH_MAX"
    This reverts commit f394d009d10487dabc94dfdcf633d69a1b682159.

--- src/include/rbh_const.h
+++ src/include/rbh_const.h
@@ -1,9 +1,7 @@
 #ifndef _RBH_CONST_H
 #define _RBH_CONST_H
 
-#include <limits.h>
-
-#define RBH_PATH_MAX    PATH_MAX
+#define RBH_PATH_MAX    1024
 #define RBH_NAME_MAX    256
 #define MAX_POOL_LEN    16
 
